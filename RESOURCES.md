942188bb2c2c8af97b9ae4c2d92a57c4788af9a1
Arpit <arpit_jain@live.com>
2022-01-11 14:42:31 +0530

    feat: ngIf and attribute directives

---
+++ RESOURCES.md
@@ -0,0 +1,4 @@
+## Further Reads
+- ngFor directive [here](https://v11.angular.io/api/common/NgForOf)
+- ngIf directive [here](https://v11.angular.io/api/common/NgIf)
+- Attribute directive class, style, bindings [here](https://v11.angular.io/guide/attribute-binding)
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
 import { Component } from '@angular/core';
 
+type Todo = {
+  id: number;
+  title: string;
+  isCompleted: boolean;
+};
+
 @Component({
   selector: 'todoapp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public title = 'Angular Todoapp';
+  public title = 'Todos';
   public todoTitle: string = '';
-  public todos: string[] = ['Learn Nodejs', 'Learn Angular', 'Learn Mongodb', 'Learn Expressjs'];
-  // todos: {id: number, todo: string, isCompleted: boolean}[] = [
-
-  // ]
+  public todos: Todo[] = [];
+  textColor: string = 'blue';
 
   public handleSubmit() {
     console.log(this.todoTitle);
-    this.todos.push(this.todoTitle);
+    const id = Math.floor(Math.random() * 50);
+    this.todos.push({
+      id: id,
+      title: this.todoTitle,
+      isCompleted: id % 2 === 0,
+    });
     this.todoTitle = '';
   }
 }
